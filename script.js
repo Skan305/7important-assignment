7229324a84d69283ac649aa1ee40dcb2eb62c30d
skhan305 <usajjad028@gmail.com>
2024-07-11 10:44:07 +0500

    add this file

---
+++ script.js
@@ -0,0 +1,80 @@
+// assignment question 
+
+// First question of the assignment
+
+// Example 1
+// let a=prompt("enter your strings ");
+// let b=prompt("enter to find the strings value")
+// let c=a.charAt(b);
+// alert("your strings number is " + c);
+
+
+
+
+// Second question of the assignment
+
+// let a = prompt("Enter words or Strings.");
+// alert(" your input .... " + a )
+// let b=prompt("enter your replacement word or strings.")
+// let c=b.replace(/b/g);
+// alert("Old string is ....."  + a)
+// alert("New word or strings...." + b)
+
+
+
+
+
+// Third question of the assignment
+
+// math.round method assignment
+// Example 1
+// let a= 8.4;
+// let b=Math.round(a);
+// console.log(b);
+
+// Example 2
+// let a= 2.8;
+// let b=Math.round(a);
+// console.log(b);
+
+
+// Fourth question of the assignment
+
+// math.ceil method assignment
+// Example 1
+
+// let a= 8.3;
+// let b=Math.ceil(a);
+// console.log(b);
+
+// Example 2
+// let a= 2.9;
+// let b=Math.ceil(a);
+// console.log(b);
+
+
+
+// five question of the assignment
+
+// math.floor method assignment
+
+// Example 1
+// let a= 2.9;
+// let b=Math.floor(a);
+// console.log(b);
+
+
+// Example 2
+// let a= 2.2;
+// let b=Math.floor(a);
+// console.log(b);
+
+
+
+
+
+
+// math.trunc mthode
+// let a= 2.7;
+// let b=Math.trunc(a);
+// console.log(b);
